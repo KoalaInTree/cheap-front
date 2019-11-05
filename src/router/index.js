ce79b39cbf3454783e68555abd338206a66f195c
BG389966 <djcao@best-inc.com>
2019-11-05 22:02:42 +0800

    修改主页路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import CheapHome from '@/components/CheapHome'
 import { Input, Button } from 'element-ui'
 
@@ -12,11 +11,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/index',
       name: 'CheapHome',
       component: CheapHome
     }
